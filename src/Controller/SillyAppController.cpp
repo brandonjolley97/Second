5a8d1b82e17d89a72b83bb9afdda7205ce3c5884
brandonjolley97 <brandonjolley97@gmail.com>
2016-01-19 09:47:52 -0700

    Created a slightly less stupid app
    learned about .h files

---
+++ src/Controller/SillyAppController.cpp
@@ -0,0 +1,39 @@
+/*
+ * SillyAppController.cpp
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: bjol7457
+ */
+
+#include "SillyAppController.h"
+#include <iostream>
+using namespace std;
+
+
+SillyAppController :: SillyAppController()
+{
+	this->count = 99;
+}
+
+void SillyAppController :: setCount(int count)
+{
+	this->count = count;
+}
+ int SillyAppController :: getCount()
+ {
+	 return this->count;
+ }
+
+ void SillyAppController :: start()
+ {
+	 cout << "In the SillyAppController\'s start method" << endl;
+	 cout << getCount() << " is the count right now" << endl;
+	 cout << "Type in a new value for count please" << endl;
+	 int tempCount;
+	 cin >> tempCount;
+	 setCount(tempCount);
+	 cout << getCount() << " is the updated count" << endl;
+ }
+
+
+
